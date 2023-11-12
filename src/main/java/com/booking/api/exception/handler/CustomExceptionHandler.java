5e8d802b499f81ae8e8d660e56f36ea5ba4c489d
flaugcand <flaugcand@gmail.comgit>
2023-11-12 14:02:44 -0300

    Code refactoring - Reducing code duplication.

--- src/main/java/com/booking/api/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/booking/api/exception/handler/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.booking.api.exception.handler;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.core.Ordered;
@@ -12,6 +13,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.booking.api.exception.ConflictException;
 import com.booking.api.exception.NotAcceptableException;
 import com.booking.api.exception.NotFoundException;
 import com.booking.api.exception.StandardError;
@@ -22,37 +24,46 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<Object> handleAllExceptions(final Exception exception, final WebRequest request) {
-		final String errorMessage = Optional.ofNullable(exception.getMessage()).orElse("Unexpected error");
-		return new ResponseEntity<>(
-				StandardError.builder().code(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(errorMessage).build(),
-				HttpStatus.INTERNAL_SERVER_ERROR);
+		return buildExceptionResponse(exception, request, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(HttpClientErrorException.BadRequest.class)
 	public final ResponseEntity<Object> handleBadRequestException(final HttpClientErrorException.BadRequest exception,
 			final WebRequest request) {
-		final String errorMessage = Optional.ofNullable(exception.getMessage()).orElse("Unexpected error");
-		return new ResponseEntity<>(
-				StandardError.builder().code(HttpStatus.BAD_REQUEST.value()).message(errorMessage).build(),
-				HttpStatus.BAD_REQUEST);
+		return buildExceptionResponse(exception, request, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(NotFoundException.class)
 	public final ResponseEntity<Object> handleNotFoundException(final NotFoundException exception,
 			final WebRequest request) {
-		final String errorMessage = Optional.ofNullable(exception.getMessage()).orElse("Unexpected error");
-		return new ResponseEntity<>(
-				StandardError.builder().code(HttpStatus.NOT_FOUND.value()).message(errorMessage).build(),
-				HttpStatus.NOT_FOUND);
+		return buildExceptionResponse(exception, request, HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(NotAcceptableException.class)
 	public final ResponseEntity<Object> handleNotAcceptableException(final NotAcceptableException exception,
 			final WebRequest request) {
-		final String errorMessage = Optional.ofNullable(exception.getMessage()).orElse("Unexpected error");
-		return new ResponseEntity<>(
-				StandardError.builder().code(HttpStatus.NOT_ACCEPTABLE.value()).message(errorMessage).build(),
-				HttpStatus.NOT_ACCEPTABLE);
+		return buildExceptionResponse(exception, request, HttpStatus.NOT_ACCEPTABLE);
 	}
 
+	@ExceptionHandler(ConflictException.class)
+	public final ResponseEntity<Object> handleNotAcceptableException(final ConflictException exception,
+			final WebRequest request) {
+		return buildExceptionResponse(exception, request, HttpStatus.CONFLICT);
+	}
+
+	private ResponseEntity<Object> buildExceptionResponse(final Exception exception, WebRequest request,
+			HttpStatus httpStatus) {
+
+		return buildExceptionResponse(exception, request, httpStatus, Optional.empty());
+	}
+
+	private ResponseEntity<Object> buildExceptionResponse(final Throwable exception, WebRequest request,
+			HttpStatus httpStatus, Optional<List<String>> details) {
+
+		final String errorMessage = Optional.ofNullable(exception.getMessage()).orElse("Unexpected error");
+
+		return new ResponseEntity<>(StandardError.builder().code(httpStatus.value()).message(errorMessage).build(),
+				httpStatus);
+
+	}
 }
