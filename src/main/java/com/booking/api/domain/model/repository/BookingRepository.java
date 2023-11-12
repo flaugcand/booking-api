490d5f831ba57e1d2d3618d1035ec0b50e8c1e01
flaugcand <flaugcand@gmail.comgit>
2023-11-12 13:59:10 -0300

    Booking Service: business rules

--- src/main/java/com/booking/api/domain/model/repository/BookingRepository.java
+++ src/main/java/com/booking/api/domain/model/repository/BookingRepository.java
@@ -1,11 +1,24 @@
 package com.booking.api.domain.model.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.booking.api.domain.model.Booking;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+	@Query("""
+			SELECT b FROM Booking b
+			WHERE (b.id !=:id OR :id IS NULL)
+				AND ((:startDate BETWEEN b.startDate AND b.endDate) OR (:endDate BETWEEN b.startDate AND b.endDate))
+			""")
+	List<Booking> findByPeriod(@Param("id") Long id, @Param("startDate") LocalDate startDate,
+			@Param("endDate") LocalDate endDate);
 
 }
--- src/main/java/com/booking/api/service/impl/BookingServiceImpl.java
+++ src/main/java/com/booking/api/service/impl/BookingServiceImpl.java
@@ -1,16 +1,24 @@
 package com.booking.api.service.impl;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 
+import com.booking.api.domain.model.Block;
 import com.booking.api.domain.model.Booking;
+import com.booking.api.domain.model.repository.BlockRepository;
 import com.booking.api.domain.model.repository.BookingRepository;
+import com.booking.api.exception.ConflictException;
+import com.booking.api.exception.NotAcceptableException;
+import com.booking.api.exception.NotFoundException;
 import com.booking.api.rest.dto.BookingDTO;
 import com.booking.api.service.BookingService;
+import com.booking.api.utils.DateUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,9 +26,11 @@ import lombok.RequiredArgsConstructor;
 public class BookingServiceImpl implements BookingService {
 
 	private final BookingRepository repository;
+	private final BlockRepository blockRepository;
 
 	@Override
 	public Long createBooking(final BookingDTO dto) {
+		bookingValidation(null, dto);
 		ObjectMapper mapper = getMapper();
 		var entity = mapper.convertValue(dto, Booking.class);
 		entity = repository.save(entity);
@@ -30,10 +40,34 @@ public class BookingServiceImpl implements BookingService {
 
 	@Override
 	public void updateBooking(final Long id, final BookingDTO dto) {
+		repository.findById(id).orElseThrow(() -> new NotFoundException("Booking not found for id: " + id));
+
+		bookingValidation(id, dto);
+
 		ObjectMapper mapper = getMapper();
 		var entity = mapper.convertValue(dto, Booking.class);
 		repository.save(entity);
 
+	}
+
+	private void bookingValidation(final Long id, final BookingDTO dto) {
+		if (Objects.isNull(dto.getStartDate()) || Objects.isNull(dto.getEndDate()))
+			throw new NotAcceptableException("The start date and end date can't be null");
+
+		if (!DateUtil.dateValidation(dto.getStartDate(), dto.getEndDate()))
+			throw new NotAcceptableException("The start date must be before the end date");
+
+		if (StringUtils.isBlank(dto.getGuestName()))
+			throw new NotAcceptableException("The guest name can't be empty");
+
+		List<Booking> bookings = repository.findByPeriod(id, dto.getStartDate(), dto.getEndDate());
+		if (bookings != null && !bookings.isEmpty())
+			throw new NotAcceptableException("The selected period is busy");
+
+		List<Block> blocks = blockRepository.findByPeriod(dto.getStartDate(), dto.getEndDate());
+
+		if (blocks != null && !blocks.isEmpty())
+			throw new NotAcceptableException("This period is blocked");
 	}
 
 	private ObjectMapper getMapper() {
@@ -44,11 +78,10 @@ public class BookingServiceImpl implements BookingService {
 
 	@Override
 	public void cancelBooking(Long id) {
-		Optional<Booking> optionalBooking = repository.findById(id);
-		if (optionalBooking.isPresent()) {
-			Booking booking = optionalBooking.get();
-			booking.setActive(Boolean.FALSE);
-			repository.save(booking);
-		}
+		Booking booking = repository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Booking not found for id: " + id));
+		booking.setActive(Boolean.FALSE);
+
+		repository.save(booking);
 	}
 }
