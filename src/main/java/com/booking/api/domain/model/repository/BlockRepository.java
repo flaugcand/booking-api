498563300908a19aa702a938fd60589edc288cdc
flaugcand <flaugcand@gmail.comgit>
2023-11-12 12:34:28 -0300

    Block Service: Business Rules.

--- src/main/java/com/booking/api/domain/model/repository/BlockRepository.java
+++ src/main/java/com/booking/api/domain/model/repository/BlockRepository.java
@@ -1,6 +1,11 @@
 package com.booking.api.domain.model.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.booking.api.domain.model.Block;
@@ -8,5 +13,10 @@ import com.booking.api.domain.model.Block;
 @Repository
 public interface BlockRepository extends JpaRepository<Block, Long> {
 	
+	@Query("""
+			SELECT b FROM Block b
+			WHERE ((:startDate BETWEEN b.startDate AND b.endDate) OR (:endDate BETWEEN b.startDate AND b.endDate))
+			""")
+	List<Block> findByPeriod(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }
-	
--- src/main/java/com/booking/api/service/impl/BlockServiceImpl.java
+++ src/main/java/com/booking/api/service/impl/BlockServiceImpl.java
@@ -1,13 +1,16 @@
 package com.booking.api.service.impl;
 
-import java.util.Optional;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 
 import com.booking.api.domain.model.Block;
 import com.booking.api.domain.model.repository.BlockRepository;
+import com.booking.api.exception.NotAcceptableException;
+import com.booking.api.exception.NotFoundException;
 import com.booking.api.rest.dto.BlockDTO;
 import com.booking.api.service.BlockService;
+import com.booking.api.utils.DateUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -21,8 +24,8 @@ public class BlockServiceImpl implements BlockService {
 
 	@Override
 	public Long createBlock(final BlockDTO dto) {
+		blockValidation(dto);
 		ObjectMapper mapper = getMapper();
-		
 		var entity = mapper.convertValue(dto, Block.class);
 		entity = repository.save(entity);
 
@@ -32,10 +35,21 @@ public class BlockServiceImpl implements BlockService {
 
 	@Override
 	public void updateBlock(final Long id, final BlockDTO dto) {
+		repository.findById(id).orElseThrow(() -> new NotFoundException("Block not found for id: " + id));
+		blockValidation(dto);
+
 		ObjectMapper mapper = getMapper();
 		var entity = mapper.convertValue(dto, Block.class);
 		repository.save(entity);
 
+	}
+
+	private void blockValidation(final BlockDTO dto) {
+		if (Objects.isNull(dto.getStartDate()) || Objects.isNull(dto.getEndDate()))
+			throw new NotAcceptableException("The start date and end date can't be null");
+
+		if (!DateUtil.dateValidation(dto.getStartDate(), dto.getEndDate()))
+			throw new NotAcceptableException("The start date must be afte the end date");
 	}
 
 	private ObjectMapper getMapper() {
@@ -47,8 +61,7 @@ public class BlockServiceImpl implements BlockService {
 
 	@Override
 	public void deleteBlock(Long id) {
-		Optional<Block> block = repository.findById(id);
-		if (block.isPresent())
-			repository.delete(block.get());
+		Block block = repository.findById(id).orElseThrow(() -> new NotFoundException("Block not found for id: " + id));
+		repository.delete(block);
 	}
 }
