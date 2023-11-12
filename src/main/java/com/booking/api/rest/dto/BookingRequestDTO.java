071430828cee3d43db05af840595b18f312f8567
flaugcand <flaugcand@gmail.comgit>
2023-11-12 14:39:45 -0300

    Get Bookings. Get All and Get By ID

--- src/main/java/com/booking/api/rest/dto/BookingRequestDTO.java
+++ src/main/java/com/booking/api/rest/dto/BookingRequestDTO.java
@@ -2,6 +2,8 @@ package com.booking.api.rest.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +16,8 @@ import lombok.Setter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-public class BookingDTO {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BookingRequestDTO {
 
 	private String guestName;
 
